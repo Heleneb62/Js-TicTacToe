f96fcab951ca3b63ef462d8b0a6b4b582a424cc0
Heleneb62 <helenebelvalpro@gmail.com>
2017-12-21 00:06:59 +0100

    mise à jour

--- js/script.js
+++ js/script.js
@@ -1,75 +1,16 @@
-var i = 1;
-function button1() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule1').className = "player1";
-} else {
-  document.getElementById('cellule1').className = "player2";
-}
-  i++
-}
-function button2() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule2').className = "player1";
-} else {
-  document.getElementById('cellule2').className = "player2";
-}
-  i++
-}
-function button3() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule3').className = "player1";
-} else {
-  document.getElementById('cellule3').className = "player2";
-}
-  i++
-}
-function button4() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule4').className = "player1";
-} else {
-  document.getElementById('cellule4').className = "player2";
-}
-  i++
-}
-function button5() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule5').className = "player1";
-} else {
-  document.getElementById('cellule5').className = "player2";
-}
-  i++
-}
-function button6() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule6').className = "player1";
-} else {
-  document.getElementById('cellule6').className = "player2";
-}
-  i++
-}
-function button7() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule7').className = "player1";
-} else if (i == 2 || i == 4 || i == 6 || i == 8) {
-  document.getElementById('cellule7').className = "player2";
-} else {
-  document.getElementById('cellule7').inneHTML = "";
-}
-  i++
-}
-function button8() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule8').className = "player1";
-} else {
-  document.getElementById('cellule8').className = "player2";
-}
-  i++
-}
-function button9() {
-  if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-  document.getElementById('cellule9').className = "player1";
-} else {
-  document.getElementById('cellule9').className = "player2";
-}
-  i++
+var joueur = 1;
+function button(numero) {
+  var td = document.getElementById('cellule'+numero);
+  if (td.className == "player1" || td.className == "player2") {
+    alert("C'est déjà joué!");
+  } else {
+    var player = "";
+    if (joueur%2 == 1) {
+    player = 1;
+  } else {
+    player = 2;
+  }
+  td.className = "player"+player;
+  joueur++;
+  }
 }
